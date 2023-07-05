103748cb49e4c3ec8f288b12b9cc05df9857fbc4
hadar <hadar933@gmail.com>
2023-07-05 12:41:38 +0300

    [requirements] added requirements.txt file

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import os
+import pickle
 from typing import Optional
 
 from langchain import HuggingFaceHub
 from langchain.embeddings.huggingface import HuggingFaceEmbeddings
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
-from langchain.vectorstores import Chroma
+from langchain.vectorstores import FAISS
 from langchain.document_loaders import UnstructuredURLLoader
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import RetrievalQAWithSourcesChain
@@ -14,8 +15,8 @@ import requests
 from xml.etree import ElementTree
 from loguru import logger
 
-os.environ['HUGGINGFACEHUB_API_TOKEN'] = '...'
-os.environ['OPENAI_API_KEY'] = '...'
+os.environ['HUGGINGFACEHUB_API_TOKEN'] = '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'
+os.environ['OPENAI_API_KEY'] = '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'
 
 
 def ai_factory(platform: str):
@@ -35,8 +36,8 @@ def ai_factory(platform: str):
             return {
                 'embedding': HuggingFaceEmbeddings(),
                 'model': HuggingFaceHub(
-                    repo_id="google/flan-t5-xl",
-                    model_kwargs={"temperature": 0, "max_length": 64}
+                    repo_id="tiiuae/falcon-7b-instruct",
+                    model_kwargs={"max_new_tokens": 500}
                 )
 
             }
@@ -56,36 +57,57 @@ class VectorDB:
                  chunk_overlap: Optional[int] = 3000,
                  url_contains: Optional[str] = None,
                  platform: Optional[str] = 'openai'):
+
         self.site_url = site_url
         self.chunk_size = chunk_size
         self.chunk_overlap = chunk_overlap
         self.pattern = url_contains
-        self.sitemap_url = f"{url}//sitemap.xml"
+        # self.sitemap_url = f"{url}//sitemap.xml"
+        self.sitemap_url = "https://ayalatours.co.il/destinations-sitemap.xml"
         self.platform = platform
-
         self.ai = ai_factory(platform)
-        self.urls = self._extract_urls_from_sitemap()
-        if self.pattern:
-            self.urls = self._filter_urls()
-        self.chunks = self._from_urls_to_chunks()
+        self.vector_db_path = os.path.join('vector_db_cache', 'vdb.pkl')
+        if os.path.exists(self.vector_db_path):  # fast load
+            self.vector_db = self._get_vector_db(from_memory=True)
+        else:  # slow load
+            self.urls = self._extract_urls_from_sitemap()
+            if self.pattern:
+                self.urls = self._filter_urls()
+            self.chunks = self._from_urls_to_chunks()
+            self.vector_db = self._get_vector_db()
+
         self.chain = self._build_chain()
 
     def _build_chain(self):
-        logger.info("Building the vector database ...")
-        docsearch = Chroma.from_documents(self.chunks, self.ai['embedding'])
+        """ returns a RetrievalQAWithSources chain given a specified llm, type and database """
         logger.info("Building the retrieval chain ...")
         chain = RetrievalQAWithSourcesChain.from_chain_type(
             llm=self.ai['model'],
             chain_type="map_reduce",
-            retriever=docsearch.as_retriever(),
+            retriever=self.vector_db.as_retriever()
         )
         return chain
+
+    def _get_vector_db(self, from_memory: bool = False):
+        """
+        returns a chroma vector database, either from memory or from scratch
+        :param from_memory: True iff the vector db was previously saved to memory
+        """
+        if from_memory:
+            logger.info(f"Loading {self.vector_db_path} from memory.")
+            with open(self.vector_db_path, "rb") as file:
+                return pickle.load(file)
+        else:
+            logger.info("Building the vector database ...")
+            vector_db = FAISS.from_documents(self.chunks, self.ai['embedding'])
+            logger.info(f"Saving vector db to memory as as {self.vector_db_path}.")
+            with open(self.vector_db_path, "wb") as file:
+                pickle.dump(vector_db, file)
+            return vector_db
 
     def _from_urls_to_chunks(self):
         """
         takes the list of urls and converts in to a dataset of chunks with given size and overlap
-        :param chunk_overlap: over-lap between consecutive chunks of data
-        :param chunk_size: number of tokens per chunk
         :return: chunks data structure
         """
         logger.info("Loading URLs content ...")
@@ -117,6 +139,7 @@ class VectorDB:
         """
          Extract all URLs from a sitemap XML string.
         :return:  A list of URLs extracted from the sitemap.
+
         """
         # Parse the XML from the string
         logger.info(f"Loading sitemap from {self.site_url} ...")
@@ -141,9 +164,10 @@ if __name__ == "__main__":
     url = 'https://ayalatours.co.il/'
     db = VectorDB(
         site_url=url,
-        url_contains='/Israel/',
+        url_contains='trip',
         platform='huggingface'
     )
 
     # Ask a question
     res = db.query("Are there any deals to Karpathops?")
+    x=2
